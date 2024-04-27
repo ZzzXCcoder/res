c8041325c13acde9567dcabae0d3f767cafb679c
ZzzXCcoder <makaka1337@list.ru>
2024-04-27 23:54:29 +0300

    спать

--- django-chatbot-main/django_chatbot/chatbot/urls.py
+++ django-chatbot-main/django_chatbot/chatbot/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
+    
 ]
--- django-chatbot-main/django_chatbot/chatbot/views.py
+++ django-chatbot-main/django_chatbot/chatbot/views.py
@@ -5,13 +5,12 @@ from openai import OpenAI;
 from django.contrib import auth
 from django.contrib.auth.models import User
 from .models import Chat
-from django.http import HttpResponse
+from .models import UserStory
 from django.utils import timezone
 
-
-
+user_story = UserStory()
 client = OpenAI(
-    api_key="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"
+    api_key="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"
 )
 
 chat_completion_0 = client.chat.completions.create(
@@ -22,7 +21,7 @@ chat_completion_0 = client.chat.completions.create(
         },
         {
             "role": "user",
-            "content": f'Меня зовут Арригал, я Маг-Эльф. Вот моя предистория: Я родом из деревени Кишки-на-ружу-ниндзя, мой отец Никита Малютин тяжело был поражен проклятьем СИШАРПА могучего демона. Мое единственное желание стать сильнее и отыскать эту нечисть, чтобы снять проклятье с моего отца!'
+            "content": f'Меня зовут Толик я жоский воин когда бью своих врагов кричу ныа !',
         }
     ],
     model="gpt-3.5-turbo",
@@ -35,14 +34,16 @@ chat_response_0 = chat_completion_0.choices[0].message.content
 
 
 def ask_openai(message):
-    answer = ""
+    user_story.history+=message
+    user_story.save
+    answer = user_story.history
     response = client.chat.completions.create(
     messages = [
         {
             "role": "system", 
-            "content" : "Ты мой Гейм Мастер в настольной ролевой игре под названием DnD. Используй систему DnD для проверок характеристик и навыков игрока где он будет бросать кубик 1к20, если выпало число меньше 10 происходит что-то плохое, если больше 10 то что-то хорошее. При этом каждое значение броска кубика ты должен прописывать. При этом веди повествование как старый рассказчик из книг. Мои соперники также бросают кубики пытаясь мне навредить и возможно даже убить",
+            "content" : "будь спокойней. не таким эксентричным. Ты мой Гейм Мастер в настольной ролевой игре под названием DnD. Используй систему DnD для проверок характеристик и навыков игрока где он будет бросать кубик 1к20, если выпало число меньше 10 происходит что-то плохое, если больше 10 то что-то хорошее. При этом каждое значение броска кубика ты должен прописывать. При этом веди повествование как старый рассказчик из книг. будь спокойнее и не торопи события а также делать более жестокий и опаснцй мир все моменты очень тшательно обыгрывай обращай внимание только на последние сообщения от пользователя учитывай что ты не союзник игроку, а наоборот пытаешся создать ему трудности и интересные ситауции Все в этом мире должны относиться ко мне с ненавистью ",
         }, 
-         {
+        {
             "role": "assistant", 
             "content" : chat_response_0,
         },
@@ -57,28 +58,29 @@ def ask_openai(message):
     ],
     model="gpt-3.5-turbo",
 
-)
+)   
+
+    user_story.save
     answer = response.choices[0].message.content
-    message.append({
-        "role": "assistant",
-        "content": char_response
-    })
-    print(answer)
+    user_story.history+=answer
+    print (user_story.history)
     return answer
 
-
+# Create your views here.
 def chatbot(request):
     chats = Chat.objects.filter(user=request.user)
-
+    user_story.user = request.user
     if request.method == 'POST':
         message = request.POST.get('message')
         response = ask_openai(message)
-
+        if user_story.history is None:  # Проверяем, является ли история пустой
+            user_story.history = message
+        else:
+            user_story.history += '\n' + message
         chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
         chat.save()
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html', {'chats': chats})
-
 
 
 def login(request):
@@ -115,6 +117,8 @@ def register(request):
             error_message = 'Password dont match'
             return render(request, 'register.html', {'error_message': error_message})
     return render(request, 'register.html')
+
+
 
 def logout(request):
     auth.logout(request)
